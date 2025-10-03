c90ad4a036a35b2806b37e469b04b8c0e160069a
d4rkra1 <noahd5689@gmail.com>
2025-10-02 19:11:16 -0500

    removed previous season config, added shooter motor for testing

--- TeamCode/src/main/java/util/robot.java
+++ TeamCode/src/main/java/util/robot.java
@@ -1,6 +1,5 @@
 package util;
 
-import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -12,9 +11,9 @@ import java.util.List;
 /*
  * sigma
  */
-@Config
+
 public class robot{
-    public DcMotorEx  liftMotor, liftMotor2;
+    public DcMotorEx  liftMotor, liftMotor2, shooter;
     // public DcMotorEx leftFront, leftRear, rightRear, rightFront,
     private List<DcMotorEx> motors;
     //public CRServo claw;
@@ -25,11 +24,6 @@ public class robot{
 
     public robot(HardwareMap hardwareMap) {
         /*
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
-         */
         intakeWrist1 = hardwareMap.get(Servo.class, "intakeWrist1Servo");
         intakeWrist2 = hardwareMap.get(Servo.class, "intakeWrist2Servo");
         claw = hardwareMap.get(Servo.class, "clawServo");
@@ -40,29 +34,15 @@ public class robot{
         specArm2 = hardwareMap.get(Servo.class, "specArmServo2");
         liftMotor = hardwareMap.get(DcMotorEx.class, "liftMotor");
         liftMotor2 = hardwareMap.get(DcMotorEx.class, "liftMotor2");
+        */
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
 
-        /*leftFront.setPower(0);
-        leftRear.setPower(0);
-        rightRear.setPower(0);
-        rightFront.setPower(0);
         //claw.setPower(0);
 
-        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        leftRear.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-*/
+        shooter.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        /*
         liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -70,7 +50,7 @@ public class robot{
         liftMotor2.setDirection(DcMotorSimple.Direction.FORWARD);
         liftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-/*
+
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
         for (DcMotorEx motor : motors) {
