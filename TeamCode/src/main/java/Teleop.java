4bae75c2149e5201a30f1e68689b337b75529ab5
Ohno17 <107664053+Ohno17@users.noreply.github.com>
2025-10-28 20:28:57 -0500

    Remove unused PIDController

--- TeamCode/src/main/java/Teleop.java
+++ TeamCode/src/main/java/Teleop.java
@@ -1,14 +1,9 @@
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
-import pedroPathing.constants.Constants;
 import util.robot;
 
 /**
